ae733ac5628afbd008b2686a81924cb481d965d4
tca19 <30314448+tca19@users.noreply.github.com>
2017-12-14 11:03:50 +0100

    day4 completed

---
+++ day4/day4.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os.path
+
+def number_valid(passphrases):
+    """Return the number of valid passphrase (tuple) if:
+         - all words must be different (1)
+         - no words should be anagrams (2)
+    """
+
+    n_valid_1 = n_valid_2 = 0
+    for passphrase in passphrases:
+        words = passphrase.split()
+        sorted_words = [''.join(sorted(w)) for w in words]
+
+        if len(set(words)) == len(words):
+            n_valid_1 += 1
+        if len(set(sorted_words)) == len(words):
+            n_valid_2 += 1
+
+    return n_valid_1, n_valid_2
+
+if __name__ == '__main__':
+    filename = "day4_passphrases.txt"
+    if not os.path.exists(filename):
+        print("ERROR. Name your input file as:", filename)
+    else:
+        passphrases = open(filename).read().strip().split("\n")
+        part_1, part_2 = number_valid(passphrases)
+        print("PART ONE:", part_1)
+        print("PART TWO:", part_2)
+
