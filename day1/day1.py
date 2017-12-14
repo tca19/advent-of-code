a865b85d353334b5bf1a57fa40dd0c396de10d31
tca19 <30314448+tca19@users.noreply.github.com>
2017-12-14 10:06:47 +0100

    day1 completed

---
+++ day1/day1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os.path
+
+def find_captcha(number, offset=1):
+    """Find the sum of digits such that number[i] = number[(i+offset) % n]"""
+
+    s = 0
+    n = len(number)
+    for i in range(n):
+        if number[i] == number[(i+offset) % n]:
+            s += int(number[i])
+
+    return s
+
+if __name__ == '__main__':
+    filename = "day1_captcha.txt"
+    if not os.path.exists(filename):
+        print("ERROR. Name your input file as:", filename)
+    else:
+        number = open(filename).read().strip()
+        part_1 = find_captcha(number)
+        print("PART ONE:", part_1)
+        part_2 = find_captcha(number, offset=len(number)//2)
+        print("PART TWO:", part_2)
+
