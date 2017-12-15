dd00591f164a0d8cd4603c4b1ebf9f40837b3618
tca19 <30314448+tca19@users.noreply.github.com>
2017-12-15 11:57:43 +0100

    day9 completed

---
+++ day9/day9.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import re
+import os.path
+
+def score(stream):
+    """Compute the score of stream."""
+
+    # remove canceled characters
+    stream = re.sub("!.", "", stream)
+
+    # get length of garbage
+    garbage = re.findall("<(.*?)>", stream)
+    garbage_length = len("".join(garbage))
+
+    # remove garbage
+    stream = re.sub("<.*?>", "", stream)
+
+    # score is increased for every new group (occurs with opening { ). For every
+    # new group, depth is incremented by 1
+    score = 0
+    depth = 0
+    for c in stream:
+        if c == "{":
+            depth += 1
+            score += depth
+        elif c == "}":
+            depth -= 1
+
+    return score, garbage_length
+
+if __name__ == '__main__':
+    filename = "day9_stream.txt"
+    if not os.path.exists(filename):
+        print("ERROR. Name your input file as:", filename)
+    else:
+        stream = open(filename).read().strip()
+        part_1, part_2 = score(stream)
+        print("PART ONE:", part_1)
+        print("PART TWO:", part_2)
